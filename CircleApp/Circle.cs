9329ccba4e9106912d81753d9de97a8f43b9d5ba
pavel99 <mailforgithub@gmail.com>
2015-05-13 10:50:25 +0600

    Intialization of Circle App

---
+++ CircleApp/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+
+
+        public double GetDiameter(double radious)
+        {
+            return 2*radious;
+        }
+
+        public double GetPerimeter(double radious)
+        {
+            return 2*Math.PI*radious;
+        }
+
+        public double GetArea(double radious)
+        {
+            return Math.PI*radious*radious;
+        }
+    }
+}
---
+++ CircleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("please enter the radius of the circle");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Circle myCircle=new Circle();
+
+            Console.WriteLine("The Diameter is :"+myCircle.GetDiameter(radius));
+            Console.WriteLine("The Perimeter is :"+myCircle.GetPerimeter(radius));
+            Console.WriteLine("The Area of Circle is :"+myCircle.GetArea(radius));
+        }
+    }
+}
